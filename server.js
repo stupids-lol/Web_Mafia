8a28dbf1bf345013faae19e2e9e49c62e48a1da1
a-8-a <cogksmf02@naver.com>
2019-02-18 20:41:05 +0900

    add input name

---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req, res){
+  res.sendFile(__dirname + '/client.html');
+});
+
+var count=0;
+
+
+io.on('connection', function(socket){
+  socket.on('hello', function(name){
+    console.log('user connected: ', socket.id);
+    console.log(name);
+  });
+  socket.on('disconnect', function(){
+    count--;
+    console.log('user disconnected: ', socket.id);
+    console.log('User : ' + count);
+  });
+
+  socket.on('user', function(name01){
+    console.log(name01);
+  });
+
+  socket.on('send message', function(name,text){
+    var msg = name + ' : ' + text;
+    console.log(msg);
+    io.emit('receive message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('User : ' + count);
+  console.log(__dirname);
+  console.log('server on!');
+});

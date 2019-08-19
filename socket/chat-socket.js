2a5b7c2febec9c2c322008b1010ff94a7febd1ee
a-8-a <cogksmf02@naver.com>
2019-08-19 17:29:56 +0900

    Merge lobby-socket to chat-socket

--- socket/chat-socket.js
+++ socket/chat-socket.js
@@ -1,12 +1,13 @@
 //chat-socket.js
 
-let count = 0; //user count
+let count = 0;  //user count
+let num = 1;    //room number
+let rooms = []; //room list
 
 module.exports = function(chat, sharedsession, session){
   chat.use(sharedsession(session), {
     autoSave:true
   });
-
   chat.on('connection', function(socket){ // if client connected
     socket.handshake.session.save();
     let name;
@@ -38,7 +39,8 @@ module.exports = function(chat, sharedsession, session){
       chat.to(room).emit('receive message', count + ' people are chatting.');
       console.log('user disconnected: ', name , getToday());
       console.log(count + ' people are chatting.');
-      socket.handshake.session.user.room = -1;
+      if (socket.handshake.session.user.join)socket.handshake.session.user.join = 0;
+      else socket.handshake.session.user.room = -1;
       socket.handshake.session.save();
     });
 
@@ -48,6 +50,36 @@ module.exports = function(chat, sharedsession, session){
         console.log(msg , getToday());
         chat.to(room).emit('receive message', msg);
       }
+    });
+
+    socket.on('create room', function(name){
+      let data = {
+        no: num,
+        name: name,
+        nop: 0,
+        leader: socket.handshake.session.user.name
+      }
+      num++;
+      rooms.push(data);
+      chat.emit('new room', [data]);
+    });
+
+    socket.on('join room', function(no){
+      console.log(no);
+      console.log('socket.join',no);
+      socket.handshake.session.user.room = no;
+      socket.handshake.session.user.join = 1;
+      console.log(socket.handshake.session.user);
+      socket.handshake.session.save();
+    });
+
+    socket.on('delete room', function(no){
+      for(let i = 0; i < rooms.length; i++){
+        if (rooms[i].no == no){
+          rooms.splice(i,1);
+        }
+      }
+      chat.emit('del room', no);
     });
   });
 }
--- socket/socket.js
+++ socket/socket.js
@@ -3,6 +3,5 @@
 module.exports = function(server, session){
   const io = require('socket.io')(server);
   const sharedsession = require("express-socket.io-session");
-  const lobby = require('./lobby-socket.js')(io.of('/chatlobby'), sharedsession, session);
-  const chat = require('./chat-socket.js')(io.of('/chat'), sharedsession, session);
+  const chat = require('./chat-socket.js')(io.of('chat'), sharedsession, session);
 }
